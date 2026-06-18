db604a6cdf31ec7d0d8865a6b5ed9e2131c46b15
chumeng <2595248810@qq.com>
2026-06-19 01:29:04 +0800

    feat(cli): add command-line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use anyhow::{Result, anyhow};
+use clap::Parser;
 use lalrpop_util::lalrpop_mod;
-use std::env::args;
 use std::fs::{read_to_string, write};
+use std::path::PathBuf;
 
 use crate::ast::ToKoopa;
 mod asm;
@@ -8,30 +10,58 @@ mod ast;
 
 lalrpop_mod!(sysy);
 
-fn main() -> Result<(), anyhow::Error> {
-    let mut args = args();
-    args.next();
-    let mode = args.next().unwrap();
-    let input = args.next().unwrap();
-    args.next();
-    let output = args.next();
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Cli {
+    #[arg(
+        value_name = "MODE",
+        value_parser = parse_mode,
+        allow_hyphen_values = true,
+        help = "Output mode: -koopa or -riscv"
+    )]
+    mode: Mode,
 
-    let input = read_to_string(input)?;
+    #[arg(value_name = "INPUT")]
+    input: PathBuf,
 
-    let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
+    #[arg(short, long, value_name = "OUTPUT")]
+    output: Option<PathBuf>,
+}
 
-    let res = match mode.as_str() {
-        "-koopa" => ast.to_koopa(),
-        "-riscv" => {
+#[derive(Clone, Copy, Debug)]
+enum Mode {
+    Koopa,
+    Riscv,
+}
+
+fn parse_mode(mode: &str) -> Result<Mode, String> {
+    match mode {
+        "-koopa" => Ok(Mode::Koopa),
+        "-riscv" => Ok(Mode::Riscv),
+        _ => Err("expected -koopa or -riscv".to_owned()),
+    }
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    let input = read_to_string(&cli.input)?;
+
+    let ast = sysy::CompUnitParser::new()
+        .parse(&input)
+        .map_err(|err| anyhow!("failed to parse input: {err:?}"))?;
+
+    let res = match cli.mode {
+        Mode::Koopa => ast.to_koopa(),
+        Mode::Riscv => {
             let koopa_ir = ast.to_koopa();
             let program = asm::str_to_program(&koopa_ir)?;
             asm::generate_asm(&program)?
         }
-        _ => unimplemented!(),
     };
 
-    if let Some(output_file) = output {
-        write(output_file, res)?;
+    if let Some(output_file) = cli.output {
+        write(&output_file, res)?;
     } else {
         println!("{}", res);
     }
