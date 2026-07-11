5b132e1b6d915cace767ee3b4398f9547a42bffb
chumeng <2595248810@qq.com>
2026-07-11 16:48:48 +0800

    feat(ir): declare and register runtime functions

---
+++ src/ir/lib_func.rs
@@ -0,0 +1,90 @@
+use crate::ast::Ident;
+
+use super::{
+    IRBuilder,
+    context::{Func, Type},
+    error::IRBuilderErr,
+    symbol::Symbol,
+};
+
+pub(super) const LIB_FUNCS: &str = r#"decl @getint(): i32
+decl @getch(): i32
+decl @getarray(*i32): i32
+decl @putint(i32)
+decl @putch(i32)
+decl @putarray(i32, *i32)
+decl @starttime()
+decl @stoptime()
+"#;
+
+struct LibFunc {
+    identifier: &'static str,
+    param_count: usize,
+    ret: Type,
+}
+
+const LIB_FUNC_SIGNATURES: &[LibFunc] = &[
+    LibFunc {
+        identifier: "getint",
+        param_count: 0,
+        ret: Type::I32,
+    },
+    LibFunc {
+        identifier: "getch",
+        param_count: 0,
+        ret: Type::I32,
+    },
+    LibFunc {
+        identifier: "getarray",
+        param_count: 1,
+        ret: Type::I32,
+    },
+    LibFunc {
+        identifier: "putint",
+        param_count: 1,
+        ret: Type::Void,
+    },
+    LibFunc {
+        identifier: "putch",
+        param_count: 1,
+        ret: Type::Void,
+    },
+    LibFunc {
+        identifier: "putarray",
+        param_count: 2,
+        ret: Type::Void,
+    },
+    LibFunc {
+        identifier: "starttime",
+        param_count: 0,
+        ret: Type::Void,
+    },
+    LibFunc {
+        identifier: "stoptime",
+        param_count: 0,
+        ret: Type::Void,
+    },
+];
+
+impl IRBuilder {
+    pub(super) fn register_lib_funcs(&mut self) -> Result<(), IRBuilderErr> {
+        if self.lib_funcs_registered {
+            return Ok(());
+        }
+
+        for lib_func in LIB_FUNC_SIGNATURES {
+            let id = Ident {
+                id: lib_func.identifier.to_owned(),
+            };
+            let func = Func {
+                identifier: lib_func.identifier.to_owned(),
+                params: vec![Type::I32; lib_func.param_count],
+                ret: lib_func.ret,
+            };
+            self.context.define_global_symbol(&id, Symbol::Func(func))?;
+        }
+
+        self.lib_funcs_registered = true;
+        Ok(())
+    }
+}
--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,5 +1,6 @@
 mod context;
 mod error;
+mod lib_func;
 mod symbol;
 
 macro_rules! emit_instruction {
@@ -19,12 +20,14 @@ mod generate;
 
 use crate::ast::CompUnit;
 use context::IRContext;
+use lib_func::LIB_FUNCS;
 
 pub use error::IRBuilderErr;
 
 #[derive(Default)]
 pub struct IRBuilder {
     context: IRContext,
+    lib_funcs_registered: bool,
 }
 
 impl IRBuilder {
@@ -33,20 +36,39 @@ impl IRBuilder {
     }
 
     pub fn gen_comp_unit(&mut self, comp_unit: &CompUnit) -> Result<String, IRBuilderErr> {
-        let mut res = String::new();
+        self.register_lib_funcs()?;
+
+        let mut output = String::from(LIB_FUNCS);
+        output.push('\n');
+        self.gen_comp_unit_body(comp_unit, &mut output)?;
+        Ok(output)
+    }
+
+    fn gen_comp_unit_body(
+        &mut self,
+        comp_unit: &CompUnit,
+        output: &mut String,
+    ) -> Result<(), IRBuilderErr> {
         match comp_unit {
             CompUnit::FuncDef(func_def) => {
-                self.context.reset_generation();
-                self.gen_func_def(func_def)?;
-                res.push_str(&self.context.take_output());
+                self.gen_function_into(func_def, output)?;
             }
             CompUnit::CompUnit(comp_unit, func_def) => {
-                res.push_str(&self.gen_comp_unit(comp_unit)?);
-                self.context.reset_generation();
-                self.gen_func_def(func_def)?;
-                res.push_str(&self.context.take_output());
+                self.gen_comp_unit_body(comp_unit, output)?;
+                self.gen_function_into(func_def, output)?;
             }
-        };
-        Ok(res)
+        }
+        Ok(())
+    }
+
+    fn gen_function_into(
+        &mut self,
+        func_def: &crate::ast::FuncDef,
+        output: &mut String,
+    ) -> Result<(), IRBuilderErr> {
+        self.context.reset_generation();
+        self.gen_func_def(func_def)?;
+        output.push_str(&self.context.take_output());
+        Ok(())
     }
 }
